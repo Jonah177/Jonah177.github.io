2942d704c1af4e60947594665d17b99344bd93b8
Jonah177 <42761712+Jonah177@users.noreply.github.com>
2020-09-07 20:26:21 -0500

    09/07/20

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ function srcButton() {
 function payButton() {
     alert("Donate Here");
 }
+
+function menu() {
+    
+    def = document.getElementsByClassName('link-def');
+    alt = document.getElementsByClassName('link-alt');
+    
+    e = document.querySelector('.hamburger');
+    if (e.classList.toggle('is-active')) {  
+        for (x of def) {
+            x.style.display = "none";
+        }
+        for (x of alt) {
+            x.style.display = "block";
+        }
+    }
+    else {
+        for (x of def) {
+            x.style.display = "block";
+        }
+        for (x of alt) {
+            x.style.display = "none";
+        }
+    }
+}
