7e68ec999dc51004017e4c57d1564491caeaf67d
Jonah177 <42761712+Jonah177@users.noreply.github.com>
2021-01-16 17:55:34 -0600

    01/16/21

--- script.js
+++ script.js
@@ -8,25 +8,49 @@ function payButton() {
     alert("Donate Here");
 }
 
-function menu() {
-    
+function menu() {  
     def = document.getElementsByClassName('link-def');
     alt = document.getElementsByClassName('link-alt');
     
-    e = document.querySelector('.hamburger');
-    if (e.classList.toggle('is-active')) {  
+    e = document.querySelector('.hamburger'); 
+    //small screen
+    if (e.classList.toggle('is-active')) { 
+        for (x of alt) {
+            x.style.display = "block";
+        }
         for (x of def) {
             x.style.display = "none";
         }
-        for (x of alt) {
-            x.style.display = "block";
-        }
     }
+    //big screen
     else {
+        for (x of alt) {
+            x.style.display = "none";
+        }
+        for (x of def) {
+            x.style.display = "block";
+        }    
+    }
+}
+
+function validate() {   
+    e = document.querySelector('.hamburger');
+    
+    //big screen
+    if (window.innerWidth >= 900) {    
+        for (x of alt) {
+            x.style.display = "none";
+        }
         for (x of def) {
             x.style.display = "block";
         }
+    }
+    //small screen
+    else if (e.classList.contains('is-active')) {
         for (x of alt) {
+            x.style.display = "block";
+        }  
+        for (x of def) {
             x.style.display = "none";
         }
     }
